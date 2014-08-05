79ab535777dc8524894f207e2293c55b0880e1a6
Cgg <clement.geiger@gmail.com>
2014-08-05 23:08:51 +0200

    datasourcemanager is just useless at this stage

---
+++ datastringer.js
@@ -0,0 +1,29 @@
+var fs = require('fs');
+
+var dataSources = [];
+var outputs = [];
+
+// load datasources
+var parsedJSON = JSON.parse(fs.readFileSync('datasources.json', 'utf8'));
+for (var i = 0; i < parsedJSON.length; i++) {
+  var DS = require(parsedJSON[i]);
+  dataSources.push(DS.dataSource);
+}
+
+// load outputs
+var parsedJSONOutput = JSON.parse(fs.readFileSync('outputs.json', 'utf8'));
+for (var i = 0; i < parsedJSONOutput.length; ++) {
+  var O = require(parsedJSONOutput[i]);
+  outputs.push(O.output);
+}
+
+var news = [];
+
+for (var i = 0; i < dataSources.length; i++) {
+  console.log('checking for news for ' + dataSources[i].name);
+
+  // TODO store the data of last update in some JSON.
+  if (dataSources[i].hasNewDataSince(new Date(Date.now()))) {
+    news.push(dataSources[i].name);
+  }
+}

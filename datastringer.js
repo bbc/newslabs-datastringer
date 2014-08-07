58c41fc499293ec794d059df90d81a827cbc154a
Cgg <clement.geiger@gmail.com>
2014-08-08 01:03:55 +0200

    datastringer.js: use objects rather than arrays to store datasources
    and outputs.

--- datastringer.js
+++ datastringer.js
@@ -1,20 +1,20 @@
 var fs = require('fs');
 
-var dataSources = [];
-var outputs = [];
+var dataSources = {};
+var outputs = {};
 
 // load datasources
 var parsedJSON = JSON.parse(fs.readFileSync('datasources.json', 'utf8'));
 for (var i = 0; i < parsedJSON.length; i++) {
   var DS = require(parsedJSON[i]);
-  dataSources.push(DS.dataSource);
+  dataSources[DS.dataSource.name] = DS.dataSource;
 }
 
 // load outputs
 var parsedJSONOutput = JSON.parse(fs.readFileSync('outputs.json', 'utf8'));
 for (var i = 0; i < parsedJSONOutput.length; i++) {
   var O = require(parsedJSONOutput[i]);
-  outputs.push(O.output);
+  outputs[O.output.name] = O.output;
 }
 
 console.log('DataSources: ' + parsedJSON);
@@ -23,12 +23,14 @@ console.log();
 
 // check which datasource has some new data to offer since last time.
 var news = [];
-for (var i = 0; i < dataSources.length; i++) {
-  console.log('checking for news for ' + dataSources[i].name);
+var dsKeys = Object.keys(dataSources);
+for (var i = 0; i < dsKeys.length; i++) {
+  var k = dsKeys[i];
+  console.log('checking for news for ' + dataSources[k].name);
 
   // TODO store the data of last update in some JSON.
-  if (dataSources[i].hasNewDataSince(new Date(Date.now()))) {
-    news.push(dataSources[i].name);
+  if (dataSources[k].hasNewDataSince(new Date(Date.now()))) {
+    news.push(dataSources[k].name);
   }
 }
 
@@ -38,13 +40,15 @@ console.log('News in: ' + news);
 // check which outputs have to be re-checked, based on which datasources have
 // new data.
 var outputsToCheck = [];
+var oKeys = Object.keys(outputs);
 for (var i = 0; i < news.length; i++) {
-  for (var j = 0; j < outputs.length; j++) {
+  for (var j = 0; j < oKeys.length; j++) {
+    var k = oKeys[j];
 
-    if (outputs[j].sources.indexOf(news[i]) != -1 &&
-        outputsToCheck.indexOf(outputs[j]) == -1) {
+    if (outputs[k].sources.indexOf(news[i]) != -1 &&
+        outputsToCheck.indexOf(k) == -1) {
 
-      outputsToCheck.push(outputs[j]);
+      outputsToCheck.push(k);
     }
   }
 }
@@ -53,9 +57,10 @@ console.log();
 console.log('Outputs inpacted by the news: ' + outputsToCheck);
 
 // now just run the 'check' method of every outputs that needs to be checked.
-for (var i = 0; i < outputs.length; i++) {
-  if (outputs[i].check(dataSources)) {
+for (var i = 0; i < outputsToCheck.length; i++) {
+  var oKey = outputsToCheck[i];
+  if (outputs[oKey].check(dataSources)) {
     // send a mail, faire tourner un girophare, etc.
-    console.log('ALERT, ouput ' + outputs[i].name + ' was triggered!');
+    console.log('ALERT, ouput ' + oKey + ' was triggered!');
   }
 }

53f41972cb101a7b8ded8a174081cd61ed8c64b1
Cgg <clement.geiger@gmail.com>
2014-08-10 19:06:01 +0200

    local-police-uk: + description

--- datasource-local-police-uk.js
+++ datasource-local-police-uk.js
@@ -4,7 +4,24 @@ var DS = require("./datasource.js");
 
 module.exports.dataSource = DS.makeDataSource(
   "local-police-uk", //name
-  "News from local force", //description
+  "#News from local force\n" + //description
+  "This source return information about a police team in a given force.\n" +
+
+  "##getData parameters\n" +
+  "`getData` accepts three parameters. The first two describe the team whose information " +
+  "you are seeking: `force` and `neighbourhood`. The last is a callback called when " +
+  "`getData` is done. Its signature is `(error, data)`. The `error` parameter is " +
+  "`undefined` when no error actually happened, and contains the error message " +
+  "otherwise. The `data` parameter contains the actual data." +
+
+  "##returned data format\n" +
+  "The source returns an object with three fields:\n\n" +
+  "- `people`: an array containing the names of the men in the team\n" +
+  "- `events`: an array containing the names of the upcoming events from the team\n" +
+  "- `priorities`: an array containing the issues the team is currently dealing with\n\n" +
+
+  "It should be noted that the data returned is always the latest available. If you " +
+  "want to compare the data to some history, you should store it somewhere yourself.",
 
   // TODO it feels like there is something to be done here.
   function hasNewsSince(date) {

0f5f7427c6332cb18bbd9611d856482510b0a0e4
Cgg <clement.geiger@gmail.com>
2014-08-10 16:45:51 +0200

    datasource-local-police-uk: actually works.

--- datasource-local-police-uk.js
+++ datasource-local-police-uk.js
@@ -1,77 +1,83 @@
-var DS = require("./datasource.js")
+var sync = require("synchronize");
+var getTheFuckingJSON = require("./utils.js").getTheFuckingJSON;
+var DS = require("./datasource.js");
 
 module.exports.dataSource = DS.makeDataSource(
   "local-police-uk", //name
   "News from local force", //description
 
+  // TODO it feels like there is something to be done here.
   function hasNewsSince(date) {
-    var threshold = new Date('2014-05-01');
-    return date > threshold;
+    return true;
   },
 
-  function getData(from, to, lat, lng) {
-
-    var dataFinal = new Array();
-
-    getNeighbourhood("http://data.police.uk/api/locate-neighbourhood?q=" + lat + "," + lng);
-
-    var yourNeighbourhood = new Array();
-
-    function getNeighbourhood(path_json) {
-      $.ajax({
-        url: path_json,
-        async: false,
-        success: doStuff(result)
-      });
+  // like callback(err, result)
+  function getData(force, neighbourhood, getDataCallback) {
+    if (!force) {
+      getDataCallback("datasource-local-police-uk: could not getData because 'force'" +
+        " was not specified.");
+      return;
     }
 
-    function doStuff(data) {
-      yourNeighbourhood.push(data.force, data.neighbourhood);
+    var dataFinal = {people: [], events: [], priorities:[]};
+    var pendingRequests = (neighbourhood ? 3 : 1);
 
-      if (yourNeighbourhood.length > 1) {
-        getNeighbourhoodData()
-      };
-    };
-
-    function getNeighbourhoodData() {
-      var baseJSON = "http://data.police.uk/api/" + yourNeighbourhood[0] + "/" + yourNeighbourhood[1];
-
-      //people
-      $.getJSON(baseJSON + "/people", function(data) {
-        var yourofficers = "";
-        for (var i = 0; i < data.length; i++) {
-          yourofficers += " " + data[i].name + ", ";
-        }
-        console.log("Your police officers are:" + yourofficers);
-        dataFinal.push({"officers": yourofficers});
-      });
-
-      //events
-      $.getJSON(baseJSON + "/events", function(data) {
-        var events = new Array();
-        if (data.length == 0) {
-          console.log("There are no announced events in your neighbourhood.");
-        } else {
-          for (var i = 0; i < data.length; i++) {
-            console.log("Events announced: " + data[i].title);
-            events.push(data[i].title);
-          }
-          dataFinal.push({"events": events});
-        }
-      });
-
-      //priorities
-      $.getJSON(baseJSON + "/priorities", function(data) {
-        var priorities = "";
-        for (var i = 0; i < data.length; i++) {
-          priorities += " " + data[i].issue + ", ";
-        }
-        console.log("Your local force's priorities are" + priorities);
-        dataFinal.push({"priorities": priorities});
-      })
+    var baseQuery = "http://data.police.uk/api/" + force;
+    if (neighbourhood) {
+      baseQuery += "/" + neighbourhood;
     }
 
-    //SHOULD RETURN ARRAY OF DATA FRAMES - WTF THAT IS
-    return dataFinal;
+    getTheFuckingJSON(baseQuery + "/people", processPeople);
+    // we need the neighbourhood param to get team events and priorities, so
+    // there is no sense in going further if we don't have it.
+    if (!neighbourhood) {
+      return;
+    }
+    getTheFuckingJSON(baseQuery + "/events", processEvents);
+    getTheFuckingJSON(baseQuery + "/priorities", processPriorities);
+
+    function processPeople(error, peopleJSON) {
+      if (error) {
+        getDataCallback(error);
+        return;
+      }
+      var people = JSON.parse(peopleJSON);
+      for (var p = 0; p < people.length; p++) {
+        dataFinal.people.push(people[p].name);
+      }
+      handleCallback();
+    }
+
+    function processEvents(error, eventsJSON) {
+      if (error) {
+        getDataCallback(error);
+        return;
+      }
+      var events = JSON.parse(eventsJSON);
+      for (var e = 0; e < events.length; e++) {
+        dataFinal.events.push(events[e].title);
+      }
+      handleCallback();
+    }
+
+    function processPriorities(error, prioritiesJSON) {
+      if (error) {
+        getDataCallback(error);
+        return;
+      }
+      var prio = JSON.parse(prioritiesJSON);
+      for (var p = 0; p < prio.length; p++) {
+        dataFinal.priorities.push(prio[p].issue);
+      }
+      handleCallback();
+    }
+
+    function handleCallback() {
+      console.log(pendingRequests);
+      pendingRequests--;
+      if (pendingRequests === 0) {
+        getDataCallback(undefined, dataFinal);
+      }
+    }
   }
 );

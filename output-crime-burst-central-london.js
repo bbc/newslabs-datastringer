2d49faaa5aecf22175d5a5ac20b0698741c561b6
Cgg <clement.geiger@gmail.com>
2014-08-12 22:59:08 +0200

    output police uk: correction

--- output-crime-burst-central-london.js
+++ output-crime-burst-central-london.js
@@ -32,14 +32,14 @@ module.exports.output = O.makeOutput(
 
     police_uk.getData(from, to, lat, lng, processStats);
 
-    function processStats(crimeStats) {
+    function processStats(crimeStatsFrames) {
       // compute the total amount of crime for each crime
       var totCrimes = [];
-      for (var i = 0; i < crimeStats.length; i++) {
+      for (var i = 0; i < crimeStatsFrames.length; i++) {
         var totalCrime = 0;
-        var types = Object.keys(crimeStats[i]);
+        var types = Object.keys(crimeStatsFrames[i].data);
         for (var t = 0; t < types.length; t++) {
-          totalCrime += crimeStats[i][t];
+          totalCrime += crimeStatsFrames[i].data[types[t]];
         }
         totCrimes.push(totalCrime);
       }

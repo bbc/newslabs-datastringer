70888a3335401838cf50e5256e5990146e899d68
Iain Collins <me@iaincollins.com>
2014-08-14 13:42:08 +0100

    Example node.js app that saves form input to a config file
    Intended to be a reference for further development (lacks any security).

---
+++ app.js
@@ -0,0 +1,44 @@
+var express = require('express'),
+    fs = require('fs'),
+    config = null;
+
+// Attempt to load config file
+try {
+    config = require(__dirname + '/config.json');
+    console.log("Existing config file found");
+} catch (e) {
+    console.log("No config file found");
+}
+
+var app = express();
+app.set('port', process.env.PORT || 3000);
+app.use(express.static(__dirname + '/wizard'));
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.post('/configure', function(req, res, next) {
+    res.setHeader('Content-Type', 'application/json; charset=utf-8');
+    // Get new config option
+    var newConfig = {
+        lat: req.body.lat,
+        lng: req.body.lng,
+        from: req.body.from,
+        to: req.body.to
+    };
+    // Write config file
+    fs.writeFile(__dirname + '/config.json', JSON.stringify(newConfig), function (err) {
+      if (err) {
+          console.log("Unable to write to config file: "+err);
+          res.json(false);
+      } else {
+          console.log("Wrote new config file");
+          config = newConfig; // Use new config
+          res.json(true);
+      }
+    });
+});
+
+app.listen(app.get('port'), function() {
+    console.log('Server started on port %d', app.get('port'));
+});
+

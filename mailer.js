56f6d4690619d088980d4076e80e20ad500ee2d4
basilesimon <basile@basilesimon.fr>
2014-08-15 20:48:48 +0100

    Mailer: fix typo

--- mailer.js
+++ mailer.js
@@ -4,7 +4,7 @@ var t = nm.createTransport();
 function sendAlert(stringerName, alertContent) {
   t.sendMail({
     from: "bot@data.string.er",
-    to: config.userEmail,
+    to: "basile@basilesimon.fr",
     subject: "ALERT for " + stringerName,
     text: "On today's run, " + stringerName + "generated an alert, with the " +
         "following content:\n" + alertContent // TODO pretty print the object
@@ -17,5 +17,5 @@ function sendAlert(stringerName, alertContent) {
 }
 
 module.exports = {
-  sendAler: sendAlert,
+  sendAlert: sendAlert,
 };

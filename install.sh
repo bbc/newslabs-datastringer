ddda7915a0b9c40b913cc56e2a3eb0ceadc8139d
Cgg <clement.geiger@gmail.com>
2014-08-15 03:21:54 +0200

    add postfix to the install script

--- install.sh
+++ install.sh
@@ -2,6 +2,13 @@
 
 echo "Installing datastringer, hang on tight..."
 
+echo "Installing postfix"
+# Install postfix to be able to send mails. Note that just apt-get installing it
+# is probably not right - ubuntu is not the only system around after all.
+# We probably just should try to find it and then kindly warn the user if it is
+# not found.
+sudo apt-get install postfix
+
 # execute the job at 12:00 each day
 (crontab -l ; echo "0 12 * * * node $(pwd)/datastringer.js") 2>&1 | sed "s/no crontab for $(whoami)//" | sort | uniq | crontab -
 

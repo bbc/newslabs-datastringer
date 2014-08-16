4c055b2ca73fd612d6a5b54930e14231078648f7
Cgg <clement.geiger@gmail.com>
2014-08-16 21:07:14 +0100

    mailer reads user email from conf file

--- mailer.js
+++ mailer.js
@@ -1,19 +1,23 @@
 var nm = require("nodemailer");
+var assetManager = require('./asset_manager.js');
 var t = nm.createTransport();
 
 function sendAlert(stringerName, alertContent) {
-  t.sendMail({
-    from: "bot@data.string.er",
-    to: "",
-    subject: "ALERT for " + stringerName,
-    text: "On today's run, " + stringerName + "generated an alert, with the " +
-        "following content:\n" + alertContent // TODO pretty print the object
-  },
-  function(err, info) {
-    if(err) {
-      console.log(err);
-    }
+  assetManager.readAsset('user-email.json', function(err, data) {
+    t.sendMail({
+      from: "bot@data.string.er",
+      to: data,
+      subject: "ALERT for " + stringerName,
+      text: "On today's run, " + stringerName + "generated an alert, with the " +
+          "following content:\n" + alertContent // TODO pretty print the object
+    },
+    function(err, info) {
+      if(err) {
+        console.log(err);
+      }
+    });
   });
+
 }
 
 module.exports = {

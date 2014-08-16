07b0de1996077aa50a67b1313e89f0525898e6db
basilesimon <basile@basilesimon.fr>
2014-08-16 15:30:30 +0100

    une tonne de truc mec.

--- mailer.js
+++ mailer.js
@@ -4,7 +4,7 @@ var t = nm.createTransport();
 function sendAlert(stringerName, alertContent) {
   t.sendMail({
     from: "bot@data.string.er",
-    to: "basile@basilesimon.fr",
+    to: "",
     subject: "ALERT for " + stringerName,
     text: "On today's run, " + stringerName + "generated an alert, with the " +
         "following content:\n" + alertContent // TODO pretty print the object

0a6d78d35b8f836b2db9ec4dec4f773a7409e046
Cgg <clement.geiger@gmail.com>
2014-08-18 12:03:30 +0100

    utils.js: make jshint happy

--- utils.js
+++ utils.js
@@ -5,12 +5,12 @@ function getTheJSON(opt, callback) {
   var json = String();
 
   var req = h.request(opt, function(response) {
-    response.on('data', function(data) {json += data});
+    response.on('data', function(data) {json += data;});
     response.on('end', function() {
       callback(undefined, json);
     });
   }).on('error', function(err){
-    callback(err, json)
+    callback(err, json);
   });
 
   req.end();

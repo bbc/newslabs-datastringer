b3924ef5a35c0d31b7bb61e5527a5cb464eaed60
oncletom <hi@oncletom.io>
2014-11-29 00:18:40 +0300

    Stringer API injection.

--- bin/datastringer.js
+++ bin/datastringer.js
@@ -7,6 +7,7 @@ var argv = require('../lib/cli-configuration');
 var Stringers = require('../lib/stringers');
 
 var stringerUseCases = Stringers.fromObject(argv);
+var stringerApi = require('../lib/api')();
 
 // now just run each use case
 console.log(stringerUseCases.length + ' use case(s) to run...');
@@ -14,7 +15,7 @@ stringerUseCases.forEach(function(stringerName){
   var stringer = Stringers.load(stringerName);
 
   var params = JSON.parse(argv[stringerName] || '{}');
-  var alertData = stringer.apply(null, params);
+  var alertData = stringer.call(null, stringerApi, params);
 
   if (alertData){
     mailer.sendAlert(argv.emailRecipient, stringerName, alertData);
